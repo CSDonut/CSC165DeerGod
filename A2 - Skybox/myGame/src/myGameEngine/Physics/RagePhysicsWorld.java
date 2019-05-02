640bbeb4633b7e9bd6d31c763196f08882585a33
Dony <donydong503@gmail.com>
2019-05-01 20:31:12 -0700

    Added character phys obj
    need to fix ground plane

--- A2 - Skybox/myGame/src/myGameEngine/Physics/RagePhysicsWorld.java
+++ A2 - Skybox/myGame/src/myGameEngine/Physics/RagePhysicsWorld.java
@@ -8,7 +8,8 @@ package myGameEngine.Physics;
         import ray.rage.scene.SceneNode;
 
 public class RagePhysicsWorld {
-    private SceneNode PlayerGroupN, RootNode, gndNode, treeNode;
+    private SceneNode PlayerGroupN, RootNode, gndNode, treeNode,
+                        characterNode;
     private PhysicsEngine physicsEng;
     private ArrayConversion arrayConversion;
     private MyGame myGameObj;
@@ -18,32 +19,37 @@ public class RagePhysicsWorld {
         this.RootNode = myGameObj.getEngine().getSceneManager().getRootSceneNode();
         this.physicsEng = physicsEng;
         this.arrayConversion = new ArrayConversion();
-
         //Game Nodes
         this.treeNode = (SceneNode)RootNode.getChild("treeNode");
         this.gndNode = (SceneNode)RootNode.getChild("TessN");
+        this.characterNode = (SceneNode)PlayerGroupN.getChild("myCubeNode");
+
     }
     public void createRagePhysicsWorld(){
         float mass = 1.0f;
         float up[] = {0,1,0};
         double[] temptf;
-        PhysicsObject gndPlaneP, treePhysObj;
+        PhysicsObject gndPlaneP, treePhysObj,characterPhysObj;
 
         //Making phys object for tree
         temptf = arrayConversion.toDoubleArray(treeNode.getLocalTransform().toFloatArray());
-        treePhysObj = physicsEng.addSphereObject(physicsEng.nextUID(), mass, temptf, 2.0f);
-//        treeP.setBounciness(1.0f);
+        treePhysObj = physicsEng.addSphereObject(physicsEng.nextUID(), 0.0f, temptf, 2.0f);
+        treePhysObj.setBounciness(1.0f);
 //        treeNode.scale(1.0f, 1.0f, 1.0f);
-//        treeNode.setLocalPosition(treeNode.getLocalPosition());
         treeNode.setPhysicsObject(treePhysObj);
+
+        //Making phys obj for character
+        temptf = arrayConversion.toDoubleArray(characterNode.getLocalTransform().toFloatArray());
+        characterPhysObj = physicsEng.addSphereObject(physicsEng.nextUID(), 0.0f, temptf, 2.0f);
+        characterPhysObj.setBounciness(1.0f);
+        characterNode.setPhysicsObject(characterPhysObj);
 
         //Making ground plane phys obj
         temptf = arrayConversion.toDoubleArray(gndNode.getLocalTransform().toFloatArray());
         gndPlaneP = physicsEng.addStaticPlaneObject(physicsEng.nextUID(),
                 temptf, up, 0.0f);
         gndPlaneP.setBounciness(1.0f);
-//        gndNode.scale(3f, .05f, 3f);
-//        gndNode.setLocalPosition(0, -7, -2);
+
         gndNode.setPhysicsObject(gndPlaneP);
 // can also set damping, friction, etc.
     }
